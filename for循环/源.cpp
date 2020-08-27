aceb31b291471733f8e5310ad608b714135579a3
yixiao-luo <1347968574@qq.com>
2020-08-27 10:57:16 +0800

    if测试

--- for循环/源.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	int i = 0;
-	int k = 0;
-	for (i = 0, k = 0; k = 0; i++, k++)
-		k++;
-	return 0;
-}
---
+++ if无匹配/源.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	/*int a = 0;
+	int b = 2;
+	if (a == 1)
+		if (b == 2)
+			printf("hehe\n");
+		else
+			printf("haha\n");
+	return 0;*/
+	int num = 1;
+	if(num=5)
+	{
+		printf("haha\n");
+	}
+
+}
